aa360208a936ad0b7258dae91ca15db5fc09a66c
Bruno Vercelli <bruno.vercelli@afya.com.br>
2024-07-07 21:52:29 -0300

    fix: axios instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,12 @@
 import Axios from 'axios';
 
+const axios = Axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 type LoginRequestParams = {
   email: string;
   password: string;
@@ -8,7 +15,7 @@ type LoginRequestParams = {
 export async function LoginRequest(params: LoginRequestParams) {
   const { email, password } = params;
 
-  const response = await Axios.post('/api/login', {
+  const response = await axios.post('/api/login', {
     email,
     password,
   });
